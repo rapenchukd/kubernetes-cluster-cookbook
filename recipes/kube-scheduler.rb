0b9f321357917a546f9a907a6258d0eb834503a6
drapenchuk <drapenchuk@bloomberg.net>
2016-05-16 13:14:39 -0400

    Further updates

--- recipes/kube-scheduler.rb
+++ recipes/kube-scheduler.rb
@@ -5,15 +5,10 @@
 # Copyright 2015-2016, Bloomberg Finance L.P.
 #
 
-file '/var/log/kube-scheduler.log' do
-  action :touch
-end
-
-template '/etc/kubernetes/inactive-manifests/scheduler.yaml' do
+template '/etc/kubernetes/scheduler' do
   mode '0640'
   source 'kube-scheduler.erb'
   variables(
-    kube_scheduler_image: node['kubernetes']['master']['scheduler-source'],
     kubernetes_api_port: node['kubernetes']['insecure']['apiport'],
     etcd_cert_dir: node['kubernetes']['secure']['directory']
   )
